f74f79fbddb5c69d752295bdf63b56472487395d
Aji Pratama <atamasite@gmail.com>
2025-11-25 19:38:33 +0700

    update thumbnail image

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,5 @@
 import { Button } from "./ui/button";
+import icon from "@/assets/images/icon.png";
 
 export default function Nav() {
   return (
@@ -7,7 +8,7 @@ export default function Nav() {
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center gap-8">
             <a href="/" className="text-xl flex items-center gap-2">
-              <img src="/icon.png" alt="KerjaRemote.net" className="w-12 h-12 rounded-full" />
+              <img src={icon.src} alt="KerjaRemote.net" className="w-12 h-12 rounded-full" />
               <span className="hidden md:inline">KerjaRemote.net</span>
             </a>
             <div className="hidden md:flex items-center gap-4">
